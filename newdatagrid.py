2b207bf8dba44ffcf11e1e2ae9e6c25292ac73db
Sridhar <sridharb.in@gmail.com>
2024-03-06 20:16:10 +0530

    Create newdatagrid.py

---
+++ newdatagrid.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+
+st.title("CSV Table with Selection")
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    select_all = st.checkbox("Select All")
+
+    if select_all:
+        selected_rows = df.index.tolist()
+    else:
+        selected_rows = st.multiselect("Select Rows", df.index.tolist())
+
+    selected_df = df.loc[selected_rows]
+    st.write(selected_df)
+
+    if st.button("Export Selected Rows"):
+        csv = selected_df.to_csv(index=False)
+        st.download_button(
+            label="Download CSV",
+            data=csv,
+            file_name="selected_rows.csv",
+            mime="text/csv",
+        )
