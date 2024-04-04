0ab11ab88a0437e005851748ddbe6bf9d4cad06d
Sridhar <sridharb.in@gmail.com>
2024-04-04 20:25:24 +0530

    Update dashtable_modal.py

--- dashtable_modal.py
+++ dashtable_modal.py
@@ -50,14 +50,31 @@ def toggle_modal(desired_page, save_n, confirm_n, cancel_n, stored_data, stored_
         raise dash.exceptions.PreventUpdate
 
     trigger_id = ctx.triggered[0]['prop_id'].split('.')[0]
+    print('-----------------')
 
-    if trigger_id == 'save-button' and stored_data != table_data and not is_open:
-        return True, stored_data, stored_page, desired_page, dash.no_update, dash.no_update
+    print('trigger_id:' + trigger_id)
+    print('is_open:' + str(is_open))
+    #print('stored_data:' + str(stored_data))
+    #print('table_data:' + str(table_data))
+    print('desired_page:' + str(desired_page))
+    #print('stored_page:' + str(stored_page))
+    print('page_current:' + str(page_current))  
+    print('save_n:' + str(save_n))
+    print('confirm_n:' + str(confirm_n))    
+    print('cancel_n:' + str(cancel_n))
+    print('-----------------')
+    
+    if trigger_id == 'save-button' and stored_data != table_data:
+        pd.DataFrame(table_data).to_csv('./data/test_data.csv', index=False)  # Save the data back to the CSV file
+        return is_open, table_data, stored_page, dash.no_update, dash.no_update, desired_page
+    #if trigger_id == 'save-button' and stored_data != table_data and not is_open:
+        #return True, stored_data, stored_page, desired_page, dash.no_update, dash.no_update
     elif trigger_id == 'table' and stored_data != table_data and not is_open:
-        return True, stored_data, stored_page, desired_page, dash.no_update, stored_page
+        return True, stored_data, stored_page, desired_page, dash.no_update, desired_page
+        #return True, stored_data, stored_page, desired_page, dash.no_update, stored_page
     elif trigger_id in ['confirm-save', 'cancel-save'] and is_open:
         if trigger_id == 'confirm-save':
-            pd.DataFrame(table_data).to_csv('your_file.csv', index=False)  # Save the data back to the CSV file
+            pd.DataFrame(table_data).to_csv('./data/test_data.csv', index=False)  # Save the data back to the CSV file
         return False, table_data if trigger_id == 'confirm-save' else stored_data, stored_page, dash.no_update, stored_data if trigger_id == 'cancel-save' else dash.no_update, desired_page
 
     return is_open, stored_data, stored_page, dash.no_update, dash.no_update, dash.no_update
